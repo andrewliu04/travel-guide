61c41b31adc450fa5ded760bed638bb7fc0fe7f3
andrewliu04 <andrewliu245@gmail.com>
2023-09-04 02:24:22 -0500

    Create map component + styles (currently w/o API key)

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,9 +1,29 @@
 import React from 'react';
+import GoogleMapReact from 'google-map-react';
+
+
+import useStyles from './styles';
 
 const Map = () => {
+    const classes = useStyles();
+
+    const coordinates = { lat: 0, lng: 0 };
 
     return (
-        <h1>Map</h1>
+        <div className={classes.mapContainer}>
+            <GoogleMapReact
+                bootstrapURLKeys={{ key: ''}}
+                defaultCenter={coordinates}
+                center={coordinates}
+                defaultZoom={14}
+                margin={[50, 50, 50, 50]}
+                options={''}
+                onChange={''}
+                onChildClick={''}
+            >
+
+            </GoogleMapReact>
+        </div>
     );
 }
 
